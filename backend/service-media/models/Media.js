277f6b823a9cee8a43849c0c5b7783a092e5248e
nielshn <niel.shn08@gmail.com>
2024-05-15 20:48:56 +0700

    first init

---
+++ backend/service-media/models/Media.js
@@ -0,0 +1,27 @@
+
+module.exports = (sequelize, DataTypes) => {
+    const Media = sequelize.define('Media', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            // field: 'updated_at',
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+    },
+        { tableName: 'media' }
+    );
+    return Media;
+}
